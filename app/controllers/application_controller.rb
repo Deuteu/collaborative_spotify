65e6baa82fbb1862362b677b5f96d1721952f0c5
Deuteu <Deuteu@users.noreply.github.com>
2019-03-16 16:12:52 +0100

    Add rescue for missing spotify authentication in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from RSpotify::MissingAuthentication do
+    render json: {error: {key: :missing_auth}}, status: :bad_gateway
+  end
 end
