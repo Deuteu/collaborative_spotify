f9e8945841082556726d425ec4644347d87b38a6
Deuteu <Deuteu@users.noreply.github.com>
2019-05-10 00:34:33 +0200

    Complete ApplicationController spec.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,6 +2,10 @@
 
 describe ApplicationController do
   describe 'rescue' do
+    def parsed_response
+      JSON.parse(response.body)
+    end
+
     context 'when an action throw a RSpotify::MissingAuthentication exception' do
       controller(ApplicationController) do
         def new_action
@@ -9,7 +13,7 @@ describe ApplicationController do
         end
       end
 
-      before(:each) do
+      before do
         routes.draw { get 'new_action' => 'anonymous#new_action' }
       end
 
@@ -17,18 +21,48 @@ describe ApplicationController do
         expect { get :new_action }.not_to raise_error
       end
 
-      let(:parsed_body) { JSON.parse(response.body) }
+      it 'renders an error json' do
+        get :new_action
+
+        expect(response.content_type).to eq('application/json')
+        expect { parsed_response }.not_to raise_error
+        expect(parsed_response.key?('error')).to be_truthy
+      end
+
+      it 'returns a bad_gateway' do
+        get :new_action
+
+        expect(response).to have_http_status(:bad_gateway)
+      end
+    end
+
+    context 'when an action throw a StandardError exception' do
+      controller(ApplicationController) do
+        def new_action
+          raise StandardError
+        end
+      end
+
+      before do
+        routes.draw { get 'new_action' => 'anonymous#new_action' }
+      end
+
+      it 'catches the error' do
+        expect { get :new_action }.not_to raise_error
+      end
 
       it 'renders an error json' do
         get :new_action
+
         expect(response.content_type).to eq('application/json')
-        expect { parsed_body }.not_to raise_error
-        expect(parsed_body.key?('error')).to be_truthy
+        expect { parsed_response }.not_to raise_error
+        expect(parsed_response.key?('error')).to be_truthy
       end
 
-      it 'returns a 502 status' do
+      it 'returns a internal_server_error' do
         get :new_action
-        expect(response).to have_http_status(:bad_gateway)
+
+        expect(response).to have_http_status(:internal_server_error)
       end
     end
   end
