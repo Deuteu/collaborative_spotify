da63bd03deec17a7980ff925a95fc5006175e9f2
Deuteu <Deuteu@users.noreply.github.com>
2019-05-09 23:32:07 +0200

    Add playlist endpoints routing config.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'spotify/search', to: 'spotify#search'
+
+  resources :playlists, param: :playlist_id, only: [:index] do
+    member do
+      get 'tracks'
+      post 'tracks', action: :add_track
+    end
+  end
 end
