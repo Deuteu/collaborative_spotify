ecba586f6c79a141ece44914fa1fa4991adda86e
Deuteu <Deuteu@users.noreply.github.com>
2019-05-11 20:47:58 +0200

    Use PlaylistsContainer in React app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import PlaylistsContainer from './components/PlaylistsContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <header className="app-header">
+                    <h1 className="app-title">Hello !</h1>
+                </header>
+                <div className="app-body">
+                    <PlaylistsContainer />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
