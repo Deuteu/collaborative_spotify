f6ca3c899518fde338935b50c0aaedb7c3dd0cb2
Deuteu <Deuteu@users.noreply.github.com>
2019-05-14 20:34:41 +0200

    Add api scope in routing config.

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,14 @@ Rails.application.routes.draw do
     end
   end
 
+  scope '/api' do
+    resources :playlists, param: :playlist_id, only: [:index] do
+      member do
+        get 'tracks'
+        post 'tracks', action: :add_track
+      end
+    end
+  end
+
   get '*path' => 'views#home'
 end
